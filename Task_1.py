4327daeceb461e6b1db0110c8d41a15f1d0d030a
alaa2001234 <79059576+alaa2001234@users.noreply.github.com>
2021-02-19 23:55:31 +0200

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("**circumference of the circle**\n")
+radius=float(input("Enter radius : "))
+const=3.14
+circumference=2*const*radius
+print(circumference)
+
+
+# In[ ]:
+
+
+
+
---
+++ Task_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("**Perimeter of the rectangle**\n")
+Length=float(input("Enter Length : "))
+width=float(input("Enter width : "))
+Perimeter=2*(Length+width)
+print(Perimeter)
+
+
+# In[ ]:
+
+
+
+
